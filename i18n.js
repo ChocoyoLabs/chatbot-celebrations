eaa759fb36e41d5d9142afd77296b4dd9c40bf27
Paulo McNally <paulomcnally@gmail.com>
2016-04-20 01:38:35 -0600

    fix typo

--- i18n.js
+++ i18n.js
@@ -1,5 +1,5 @@
 module.exports = {
-  today: [
+  wikipedia: [
     {
       text: 'Encontré información al respecto en wikipedia mirá donde dice Nicaragua, hay como 25 festividades y celebraciones: https://es.wikipedia.org/wiki/Anexo:Festividades_y_celebraciones',
     },
